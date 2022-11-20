51c9254ffb63ea11d6a892b41e79314ab39c1c4c
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-20 20:42:38 +0100

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -10,10 +10,10 @@ col2.image(Image.open('Image/image.png'))
 st.header('Anuoluwapo Balogun | Data Analyst')
 
 st.info('A Certified Data analyst and a self taught Data scientist and engineer specialized in sales and consultancy')
-col2.write('Email: ifeoluwapobalogun1@gmail.com | Phone: +2349090251869')
+col1.write('Email: ifeoluwapobalogun1@gmail.com | Phone: +2349090251869')
 col2.write('PostgreSQL | Microsoft SQL Server | MongoDB PowerBI | Tableau Python | R |'
                       ' Excel | SAS | SPSS | FaunaDB | CockroachDB Snowflake | AWS | GCP | Azure')
-st.write('ML Algorithmn | A/B Testing | Data Wrangling |'
+col3.write('ML Algorithmn | A/B Testing | Data Wrangling |'
          ' Analytics | Data Preprocessing | Data Visualization'
          ' Exploratory Data Analysis | Fraud Analysis |'
          ' Hypothesis Testing | Sales Analysis | Consulting')
