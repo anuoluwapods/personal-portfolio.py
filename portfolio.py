9a153f10f5a134ef36b2a5cf9934a2f49475d52c
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-20 18:56:20 +0100

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -5,7 +5,7 @@ from PIL import Image
 load_css()
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('image/image.png'))
+col2.image(Image.open('Image/image.png'))
 
 st.header('Anuoluwapo Balogun | Data Analyst')
 
