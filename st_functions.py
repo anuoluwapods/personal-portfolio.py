30c9befc546cde2dc4e7a289fb60938b3404bae8
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-20 01:04:49 +0100

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -46,7 +46,7 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
-     elif icon == 'hashnode':
+    elif icon == 'hashnode':
         button_code = f'''
         <p>
             <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
